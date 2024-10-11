70ba7d0e297880ca0f55b59788180e85acd788a6
saloni11042 <saloni11042@gmail.com>
2024-10-11 16:44:54 +0530

    css added

--- NodeProject/routes/admin.js
+++ NodeProject/routes/admin.js
@@ -6,12 +6,8 @@ const router = express.Router();
 
 
 router.get('/add-product',(req, res, next)=>{
-
     res.sendFile(path.join(__dirname, '../', 'views','add-product.html'))
-
 });
-
-
     router.post('/add-product',(req, res, next)=>{
     console.log(req.body)
     res.redirect('/');
